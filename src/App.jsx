9b160002bf919b1b27101ec02098204db33ed164
mihaisacalianu <mihaisacalianu@gmail.com>
2025-05-13 14:34:31 +0100

    project pushed to gitHub

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-9xl'>app is ready</h1>
+    </>
+  )
+}
+
+export default App
