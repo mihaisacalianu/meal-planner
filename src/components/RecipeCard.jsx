de78732558abc46f43d89fe7e6d947243ab66d3b
mihaisacalianu <mihaisacalianu@gmail.com>
2025-05-14 15:27:10 +0100

    i can fecth data from mockApi and redirect customers to create a meal page if no meals available

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,17 +1,17 @@
 import { IoTime } from "react-icons/io5";
 import { PiCookingPotFill } from "react-icons/pi";
 
-function RecipeCard() {
+function RecipeCard({meal}) {
   return (
     <article className='flex flex-col rounded-xl shadow-lg w-[30%] h-[45%]items-center' >
       <img src="https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Air-Fryer-Baked-Potatoes-4fde688.jpg?quality=90" alt="recipe card image" className='w-full rounded-t-xl' />
       <div className="flex flex-col w-[100%] mt-2" aria-label='meal description container'>
-        <h3 className='px-6 text-xl'><strong>Jacket Potato</strong></h3>
+        <h3 className='px-6 text-xl'><strong>{meal.title}</strong></h3>
         <div className="flex items-center gap-4 border-b-1 border-gray-200 pb-2 px-6 text-gray-400" aria-label='meal additional information'>
           <IoTime className="text-green-400"/>
-          <p>20 minutes</p>
+          <p>{meal.time} minutes</p>
           <PiCookingPotFill className="text-orange-400"/>
-          <p>9 ingredients</p>
+          <p>{meal.serving} ingredients</p>
         </div>
       </div>
       <div className=" flex gap-2 text-gray-400 justify-center h-[50px]">
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 import RecipeCard from '../components/RecipeCard'
+import NoRecipe from './NoRecipe';
 
 function Recipes() {
+
+  const [isFetching, setIsFetching] = useState(false);
+  const [meals, setMeals] = useState([]);
+
+useEffect(()=>{
+  setIsFetching(true);
+  async function fetchMeals(){
+    const response = await fetch("https://6823283065ba058033957fbd.mockapi.io/meals");
+    const resData = await response.json();
+    setMeals(resData);
+    setIsFetching(false);
+  }
+  fetchMeals();
+},[]);
+
+
   return (
     <section className=' flex flex-wrap gap-4 col-start-2  row-start-2 m-5 rounded-xl shadow-lg p-5 justify-center'>
-      <RecipeCard/>
-      <RecipeCard/>
-      <RecipeCard/>
-      <RecipeCard/>
-      <RecipeCard/>
-      <RecipeCard/>
+      {isFetching && <p>Meals are loading</p>}
+      {!isFetching && meals.length === 0 && <NoRecipe/>}
+      {meals.length > 0 && meals.map((meal)=> <RecipeCard key={meal.id} meal={meal}/>)}
     </section>
   )
 }
